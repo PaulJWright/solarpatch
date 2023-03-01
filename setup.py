265fc189f7d89040f493d1ea0b516ffac00dac6f
Paul Wright <paul@pauljwright.co.uk>
2023-03-01 21:57:53 +0000

    correct version.py gen for black

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ VERSION_TEMPLATE = """
 # version, so we catch the generic 'Exception'.
 try:
     from setuptools_scm import get_version
+
     __version__ = get_version(root='..', relative_to=__file__)
+
 except Exception:
     __version__ = '{version}'
 """.lstrip()
